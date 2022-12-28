e487815bea26c6e784e4e08c021b22e3cb1f1d9e
FranklinMiranda <fdm61344@gmail.com>
2022-12-28 16:46:10 -0500

    CM

--- react-fetchapi/src/Components/Data.js
+++ react-fetchapi/src/Components/Data.js
@@ -1,2 +1,49 @@
-import React from "react";
+import React from 'react';
 
+// API endpoint URL https://jsonplaceholder.typicode.com/users
+
+class Data extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false,
+    };
+  }
+
+  // Lifecycle Method ComponentDidMount is used to execute fetch when the Data Component is first rendered
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          items: data,
+          DataisLoaded: true,
+        });
+      });
+  }
+
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1>Please wait some more time....</h1>
+        </div>
+      );
+
+    return (
+      <div className="Data">
+        <h1>Fetch data from an API in React</h1>
+        {items.map((item) => (
+          <ol key={item.id}>
+            User_Name: {item.username}
+            Full_Name: {item.name}
+            User_Email: {item.email}
+          </ol>
+        ))}
+      </div>
+    );
+  }
+}
