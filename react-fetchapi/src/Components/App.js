a8328b212f1f80aba66cabe7a044ef600b65cd33
FranklinMiranda <fdm61344@gmail.com>
2022-12-29 14:27:43 -0500

    CM

--- react-fetchapi/src/Components/App.js
+++ react-fetchapi/src/Components/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Data from './FetchAPIData';
 import AxiosData from './AsyncAwaitData'
 import Hooks from './Hooks'
+import Message from './UseState';
 
 class App extends React.Component {
   render() {
     return <div className="App">
-        <Data/>
-        <AxiosData/>
-        <Hooks/>
+        <Message/>
     </div>;
   }
 }
--- react-fetchapi/src/Components/UseState.js
+++ react-fetchapi/src/Components/UseState.js
@@ -1,2 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
+function Message() {
+  const [message, setMessage] = useState('Hello');
+
+  function handleClick() {
+    setMessage('Bye');
+  }
+
+  return (
+    <div className="Message-Div">
+      <h1>{message}</h1>
+      <button onClick={handleClick}>Change Message</button>
+    </div>
+  );
+}
+
+export default Message;
