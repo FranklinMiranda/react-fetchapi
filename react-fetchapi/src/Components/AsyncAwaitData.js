a4ac8cb2319a3d004a95f1b616b27e9b6efb4430
FranklinMiranda <fdm61344@gmail.com>
2022-12-28 20:25:39 -0500

    CM

--- react-fetchapi/src/Components/AsyncAwaitData.js
+++ react-fetchapi/src/Components/AsyncAwaitData.js
@@ -1,4 +1,28 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
 // This is a boilerplate for fetching Data using Async/Await
+// This code also uses React hooks in simple functional components
 
+function AxiosData() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  React.useEffect(() => {
+    const loadPost = async () => {
+      setLoading(true);
+
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts/');
+
+      setPosts(response.data);
+
+      setLoading(false);
+    };
+
+    loadPost();
+  }, []);
+
+  return <div className="AxiosData">{loading ? <h4>Loading...</h4> : posts.map((item) => <h4>{item.title}</h4>)}</div>;
+}
+
+export default AxiosData
