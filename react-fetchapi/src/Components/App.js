0ef6be7ba6d54d7dae12ec52ad6b0ab1c6a1fa89
FranklinMiranda <fdm61344@gmail.com>
2022-12-29 15:51:02 -0500

    CM

--- react-fetchapi/src/Components/App.js
+++ react-fetchapi/src/Components/App.js
@@ -3,15 +3,17 @@ import React from 'react';
 // import AxiosData from './AsyncAwaitData'
 // import Hooks from './Hooks'
 import Message from './UseState';
+import Counter from './UseEffect';
 
 class App extends React.Component {
   render() {
-    return <div className="App">
-        <Message/>
-    </div>;
+    return (
+      <div className="App">
+        <Message />
+        <Counter />
+      </div>
+    );
   }
 }
 
 export default App;
-
-
--- react-fetchapi/src/Components/UseEffect.js
+++ react-fetchapi/src/Components/UseEffect.js
@@ -1,2 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div className="Counter-Div">
+      <p>You clicked {count} times!</p>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
+    </div>
+  );
+}
+
+export default Counter
